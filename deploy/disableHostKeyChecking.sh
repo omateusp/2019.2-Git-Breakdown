c841e047d5ac50792368fe88f94107b4299dfc0f
Mateus Oliveira <o.mateus.p@gmail.com>
2019-10-07 21:31:33 -0300

    das

--- deploy/disableHostKeyChecking.sh
+++ deploy/disableHostKeyChecking.sh
@@ -13,6 +13,6 @@
 
 # any future command that fails will exit the script
 set -e
-sudo mkdir -p ~/.ssh
-sudo touch ~/.ssh/config
-sudo echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
+mkdir -p ~/.ssh
+touch ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" >> ~/.ssh/config
